1390600aae966b8062ce8715bb7b303169e52a13
nirinium <wills.colton@gmail.com>
2019-04-18 21:28:21 -0400

    chg

--- benjiStockApp.py
+++ benjiStockApp.py
@@ -6,6 +6,8 @@ import PIL
 from PIL import Image, ImageDraw, ImageFont
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
+#import plotTicker
 
 #CONFIG
 randomIntz = str(random.randrange(1, 100000)) #numpy.random.randint(4, size=10)
@@ -14,29 +16,31 @@ r = requests.get("https://finance.yahoo.com/quote/%5EDJI/components/")
 stocks = pd.read_html("https://finance.yahoo.com/quote/%5EDJI/components/", header=0, parse_dates=["Symbol"])
 tickerFile = 'ticker_' + str(randomIntz) + '.png'
 
-#
+##################
+
 def drawTicker():
 
     fnt = ImageFont.truetype("consola.ttf", 12)
     im = Image.new('RGBA', (800, 800), 'white')
     draw = ImageDraw.Draw(im)
     draw.text((25, 25), str(stocks), fill = 'black', font=fnt)
-    draw.text((0, 5), 'CSV Export of Ticker', fill = 'black', font=fnt, anchor="center")
+    draw.text((5, 5), 'CSV export of ticker using PIL Library', fill = 'black', font=fnt, anchor="center")
     im.save(tickerFile)
 
-drawTicker()
 
 def mainF():
-    # Request sent for while loop
-
 # While the website request is "OK"
     if (r.status_code == 200):
         # Grabs the table data from the website and parses
         df = pd.DataFrame(stocks)        
         df.to_csv('stock.csv')
-        print(stocks[0])
+        
+    print(stocks[0])
+
+    
 
 mainF()
 
+drawTicker()
 
 #exit();
---
+++ plotTicker.py
@@ -0,0 +1,33 @@
+import csv, time, requests, os, numpy.random, random
+from datetime import datetime
+import pathlib as pLib
+import pandas as pd
+import PIL
+from PIL import Image, ImageDraw, ImageFont
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+y = np.random.normal(0, 1, 10000).cumsum(axis=0)
+x = np.arange(y.size)
+
+fig, ax = plt.subplots()
+line, = ax.plot([], [], '.')
+ax.margins(0.05)
+
+def init():
+    line.set_data(x[:2],y[:2])
+    return line,
+
+def animate(i):
+    i = min(i, x.size)
+    xdata = x[:i]
+    ydata = y[:i]
+    line.set_data(xdata, ydata)
+    ax.relim()
+    ax.autoscale()
+    return line,
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, interval=25)
+
+plt.show()
